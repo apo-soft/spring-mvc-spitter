41e0f8fb0f8c3a9fdedfe1cb4ade24b5060e9a24
liujian1 <liujian1@gomeholdings.com>
2016-09-23 04:49:54 +0800

    删除warnings

--- spitter-persistence-jdbc-dao-support/src/main/java/com/habuma/spitter/persistence/SimpleJdbcSupportSpitterDao.java
+++ spitter-persistence-jdbc-dao-support/src/main/java/com/habuma/spitter/persistence/SimpleJdbcSupportSpitterDao.java
@@ -12,90 +12,85 @@ import com.habuma.spitter.domain.Spittle;
 
 public class SimpleJdbcSupportSpitterDao extends JdbcDaoSupport implements SpitterDao {
 
-	private static final String SQL_INSERT_SPITTER = "insert into spitter (username, password, fullname, email, update_by_email) values (?, ?, ?, ?, ?)";
+    private static final String SQL_INSERT_SPITTER = "insert into spitter (username, password, fullname, email, update_by_email) values (?, ?, ?, ?, ?)";
 
-	private static final String SQL_UPDATE_SPITTER = "update spitter set username = ?, password = ?, fullname = ?, set email = ?"
-			+ "where id = ?";
+    private static final String SQL_UPDATE_SPITTER = "update spitter set username = ?, password = ?, fullname = ?, set email = ?" + "where id = ?";
 
-	private static final String SQL_SELECT_SPITTER = "" + "select id, username, password, fullname, email from spitter";
+    private static final String SQL_SELECT_SPITTER = "" + "select id, username, password, fullname, email from spitter";
 
-	private static final String SQL_SELECT_SPITTER_BY_ID = SQL_SELECT_SPITTER + " where id=?";
+    private static final String SQL_SELECT_SPITTER_BY_ID = SQL_SELECT_SPITTER + " where id=?";
 
-	// <start id="java_getSpitterById" />
-	public Spitter getSpitterById(long id) {
-		Object[] obja = new Object[1];
-		obja[0] = id;
-		/**
-		 * Spring 2.5, 3.x 使用
-		 */
-		RowMapper<Spitter> mapper = new RowMapper<Spitter>() {
-			public Spitter mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Spitter spitter = new Spitter();
-				spitter.setId(rs.getLong(1));
-				spitter.setUsername(rs.getString(2));
-				spitter.setPassword(rs.getString(3));
-				spitter.setFullName(rs.getString(4));
-				spitter.setEmail(rs.getString(5));
-				return spitter;
-			}
-		};
+    // <start id="java_getSpitterById" />
+    public Spitter getSpitterById(long id) {
+        Object[] obja = new Object[1];
+        obja[0] = id;
+        /**
+         * Spring 2.5, 3.x 使用
+         */
+        RowMapper<Spitter> mapper = new RowMapper<Spitter>() {
+            public Spitter mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Spitter spitter = new Spitter();
+                spitter.setId(rs.getLong(1));
+                spitter.setUsername(rs.getString(2));
+                spitter.setPassword(rs.getString(3));
+                spitter.setFullName(rs.getString(4));
+                spitter.setEmail(rs.getString(5));
+                return spitter;
+            }
+        };
 
-		return getJdbcTemplate().queryForObject(SQL_SELECT_SPITTER_BY_ID, obja, mapper);
-	}
-	// <end id="java_getSpitterById" />
+        return getJdbcTemplate().queryForObject(SQL_SELECT_SPITTER_BY_ID, obja, mapper);
+    }
+    // <end id="java_getSpitterById" />
 
-	// <start id="java_addSpitter" />
-	public void addSpitter(Spitter spitter) {
-		getJdbcTemplate().update(SQL_INSERT_SPITTER, spitter.getUsername(), spitter.getPassword(),
-				spitter.getFullName(), spitter.getEmail(), spitter.isUpdateByEmail());
-		spitter.setId(queryForIdentity());
-	}
-	// <end id="java_addSpitter" />
+    // <start id="java_addSpitter" />
+    public void addSpitter(Spitter spitter) {
+        getJdbcTemplate().update(SQL_INSERT_SPITTER, spitter.getUsername(), spitter.getPassword(), spitter.getFullName(), spitter.getEmail(),
+                spitter.isUpdateByEmail());
+        spitter.setId(queryForIdentity());
+    }
+    // <end id="java_addSpitter" />
 
-	public void saveSpitter(Spitter spitter) {
-		getJdbcTemplate().update(SQL_UPDATE_SPITTER, spitter.getUsername(), spitter.getPassword(),
-				spitter.getFullName(), spitter.getEmail(),
+    public void saveSpitter(Spitter spitter) {
+        getJdbcTemplate().update(SQL_UPDATE_SPITTER, spitter.getUsername(), spitter.getPassword(), spitter.getFullName(), spitter.getEmail(),
 
-				spitter.getId());
-	}
+                spitter.getId());
+    }
 
-	// <start id="java_queryForIdentity" />
-	private long queryForIdentity() {
-		return getJdbcTemplate().queryForObject("call identity()", Long.class);
-	}
-	// <end id="java_queryForIdentity" />
+    // <start id="java_queryForIdentity" />
+    private long queryForIdentity() {
+        return getJdbcTemplate().queryForObject("call identity()", Long.class);
+    }
+    // <end id="java_queryForIdentity" />
 
-	public List<Spittle> getRecentSpittle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Spittle> getRecentSpittle() {
 
-	public void saveSpittle(Spittle spittle) {
-		// TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    public void saveSpittle(Spittle spittle) {
 
-	public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	public Spitter getSpitterByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
+        return null;
+    }
 
-	public void deleteSpittle(long id) {
-		throw new UnsupportedOperationException();
-	}
+    public Spitter getSpitterByUsername(String username) {
 
-	public Spittle getSpittleById(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        return null;
+    }
 
-	public List<Spitter> findAllSpitters() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void deleteSpittle(long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Spittle getSpittleById(long id) {
+
+        return null;
+    }
+
+    public List<Spitter> findAllSpitters() {
+        return null;
+    }
 }
--- spitter-service-declarative-tx/src/main/java/com/habuma/spitter/service/SpitterServiceImpl.java
+++ spitter-service-declarative-tx/src/main/java/com/habuma/spitter/service/SpitterServiceImpl.java
@@ -18,64 +18,62 @@ import com.habuma.spitter.persistence.SpitterDao;
 @Transactional(propagation = Propagation.REQUIRED)
 public class SpitterServiceImpl implements SpitterService {
 
-	// <start id="java_addSpittle" />
-	public void saveSpittle(Spittle spittle) {
-		spitterDao.saveSpittle(spittle);
-	}
-	// <end id="java_addSpittle" />
+    // <start id="java_addSpittle" />
+    public void saveSpittle(Spittle spittle) {
+        spitterDao.saveSpittle(spittle);
+    }
+    // <end id="java_addSpittle" />
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<Spittle> getRecentSpittles(int count) {
-		List<Spittle> recentSpittles = spitterDao.getRecentSpittle();
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public List<Spittle> getRecentSpittles(int count) {
+        List<Spittle> recentSpittles = spitterDao.getRecentSpittle();
 
-		reverse(recentSpittles);
+        reverse(recentSpittles);
 
-		return recentSpittles.subList(0, min(49, recentSpittles.size()));
-	}
+        return recentSpittles.subList(0, min(49, recentSpittles.size()));
+    }
 
-	public void saveSpitter(Spitter spitter) {
-		if (spitter.getId() == null) {
-			spitterDao.addSpitter(spitter);
-		} else {
-			spitterDao.saveSpitter(spitter);
-		}
-	}
+    public void saveSpitter(Spitter spitter) {
+        if (spitter.getId() == null) {
+            spitterDao.addSpitter(spitter);
+        } else {
+            spitterDao.saveSpitter(spitter);
+        }
+    }
 
-	@Autowired
-	SpitterDao spitterDao;
+    @Autowired
+    SpitterDao spitterDao;
 
-	public Spitter getSpitter(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Spitter getSpitter(long id) {
+        return null;
+    }
 
-	public void startFollowing(Spitter follower, Spitter followee) {
-		// TODO Auto-generated method stub
+    public void startFollowing(Spitter follower, Spitter followee) {
 
-	}
+    }
 
-	public Spitter getSpitter(String username) {
-		return spitterDao.getSpitterByUsername(username);
-	}
+    public Spitter getSpitter(String username) {
+        return spitterDao.getSpitterByUsername(username);
+    }
 
-	public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
-		return spitterDao.getSpittlesForSpitter(spitter);
-	}
+    public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
+        return spitterDao.getSpittlesForSpitter(spitter);
+    }
 
-	public List<Spittle> getSpittlesForSpitter(String username) {
-		Spitter spitter = spitterDao.getSpitterByUsername(username);
-		return getSpittlesForSpitter(spitter);
-	}
+    public List<Spittle> getSpittlesForSpitter(String username) {
+        Spitter spitter = spitterDao.getSpitterByUsername(username);
+        return getSpittlesForSpitter(spitter);
+    }
 
-	public List<Spitter> getAllSpitters() {
-		return spitterDao.findAllSpitters();
-	}
+    public List<Spitter> getAllSpitters() {
+        return spitterDao.findAllSpitters();
+    }
 
-	public void deleteSpittle(long id) {
-		spitterDao.deleteSpittle(id);
-	}
+    public void deleteSpittle(long id) {
+        spitterDao.deleteSpittle(id);
+    }
 
-	public Spittle getSpittleById(long id) {
-		return spitterDao.getSpittleById(id);
-	}
+    public Spittle getSpittleById(long id) {
+        return spitterDao.getSpittleById(id);
+    }
 }
